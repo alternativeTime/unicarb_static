01dc15615d422d1d5f48e4af865d05d538e9b29c
Marc Ziani de Ferranti <mezdef@gmail.com>
2012-10-29 15:47:02 +1100

    updated icons and javascript for browse

---
+++ public/javascripts/application.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $("#querydiseasecheck").click(function() {
+    $("#querydiseasehide").toggle();
+  });
+
+  $("#querymasscheck").click(function() {
+    $("#querymasshide").toggle();
+  });
+
+  $("#querycompositioncheck").click(function() {
+    $("#querycompositionhide").toggle();
+  });
+
+  $("#queryattachedcheck").click(function() {
+    $("#queryattachedhide").toggle();
+  });
+
+  $("#queryproteincheck").click(function() {
+    $("#queryproteinehide").toggle();
+  });
+
+  $("#querytaxonomycheck").click(function() {
+    $("#querytaxonomyhide").toggle();
+  });
+
+  $("#querybiologicalcheck").click(function() {
+    $("#querybiologicalhide").toggle();
+  });
+
+  $("#querysourcecheck").click(function() {
+    $("#querysourcehide").toggle();
+  });
+
+});
