c7e89b9d3b079d24f45ce050ba36c8df37db1834
Marc Ziani de Ferranti <mezdef@gmail.com>
2012-12-04 12:31:22 +1100

    cleaning up browse page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,4 +32,23 @@ $(document).ready(function(){
     $("#querysourcehide").toggle();
   });
 
+  $("input[name='glycosuitedbSearch']").change(function() {
+    if ($(this).val() === "Taxonomy") { $("#glycosuitedbTaxonomy").show(); } else { $("#glycosuitedbTaxonomy").hide(); }
+    if ($(this).val() === "Localisation") { $("#glycosuitedbLocalisation").show(); } else { $("#glycosuitedbLocalisation").hide(); }
+    if ($(this).val() === "ProteinLocalisation") { $("#glycosuitedbAssociation").show(); } else { $("#glycosuitedbAssociation").hide(); }
+    if ($(this).val() === "Composition") { $("#glycosuitedbComposition").show(); } else { $("#glycosuitedbComposition").hide(); }
+  });
+
+  $("input[name='eurocarbdbSearch']").change(function() {
+    if ($(this).val() === "Taxonomy") { $("#eurocarbdbTaxonomy").show(); } else { $("#eurocarbdbTaxonomy").hide(); }
+    if ($(this).val() === "Localisation") { $("#eurocarbdbLocalisation").show(); } else { $("#eurocarbdbLocalisation").hide(); }
+    if ($(this).val() === "ProteinAssociation") { $("#eurocarbdbAssociation").show(); } else { $("#eurocarbdbAssociation").hide(); }
+  });
+
+  $("input[name='glycobaseSearch']").change(function() {
+    if ($(this).val() === "Taxonomy") { $("#glycobaseTaxonomy").show(); } else { $("#glycobaseTaxonomy").hide(); }
+    if ($(this).val() === "Localisation") { $("#glycobaseLocalisation").show(); } else { $("#glycobaseLocalisation").hide(); }
+    if ($(this).val() === "ProteinLocalisation") { $("#glycobaseAssociation").show(); } else { $("#glycobaseAssociation").hide(); }
+    if ($(this).val() === "Composition") { $("#glycobaseComposition").show(); } else { $("#glycobaseComposition").hide(); }
+  });
 });
