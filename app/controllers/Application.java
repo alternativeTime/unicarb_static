f5da2b41134f385216a0b0fd7940012f20f9368b
Matthew <matthew@firestarter.local>
2013-04-01 23:12:41 +1100

    addition of login security, pubmed start

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,6 +14,7 @@ import models.*;
 import play.db.ebean.*;
 import play.data.format.*;
 import play.data.validation.*;
+import static play.data.Form.*;
 
 
 import com.avaje.ebean.*; //dont think this should be here due to SqlRow
@@ -450,13 +451,14 @@ public class Application extends Controller {
     }
 
     public static Result login() {
-      return ok ( login.render() );
+      return ok ( login.render(form(Login.class)) );
     }
 
     public static Result workflows() {
       return ok ( workflows.render() );
     }
 
+    @Security.Authenticated(Secured.class)
     public static Result massspec() {
       return ok ( mass_spec.render() );
     }
@@ -592,6 +594,49 @@ public class Application extends Controller {
 			tissuesummary.render("Tissue Summary", databaseReference, tissueresult)
 			);
 	} 
-	
+
+
+	// -- Authentication
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+        public String validate() {
+            if(User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
+
+    }
+
+     /**
+     * Handle login form submission.
+     */
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if(loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session("email", loginForm.get().email);
+            return redirect(
+               routes.Application.index() 
+            );
+        }
+    }
+
+    /**
+     * Logout and clean the session.
+     */
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+           routes.Application.index() 
+        );
+    }
+
 }
 
---
+++ app/controllers/Pubmed.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import java.util.*;
+
+import play.mvc.*;
+import play.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+
+import views.html.*;
+
+import models.*;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+
+public class Pubmed extends Controller {
+
+	public static Result getpubmed() {
+
+		List<Method> method = Ebean.find(Method.class).findList();
+		List<TissueTaxonomy> tissue = Ebean.find(TissueTaxonomy.class).findList();
+                List<Disease> disease = Ebean.find(Disease.class).findList();
+		return ok(pubmed.render("pubmed", method, tissue, disease) );	
+
+	}
+	
+}
+
