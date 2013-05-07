555437244f2c165d5e1217b435bf7cfa9ee91575
Matthew Campbell <matthew.campbell1980@gmail.com>
2013-05-07 17:31:13 +1000

    extending swiss prot link but needs a lot of tidying

--- app/controllers/UniprotConnection.java
+++ app/controllers/UniprotConnection.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 import play.mvc.*;
 import play.*;
-import play.cache.Cache;
 import play.cache.*;
 
 import views.html.*;
@@ -42,24 +41,24 @@ import uk.ac.ebi.kraken.uuw.services.remoting.RemoteDataAccessException;
 
 public class UniprotConnection extends Controller {
 
-	
+
 	public static List<String>EntryRetrievalExample(String accession){
 
-	
-	ArrayList arrayFunction = new ArrayList();
-	List arrayPTM = new ArrayList();
-        //Create entry retrieval service
-        EntryRetrievalService entryRetrievalService = UniProtJAPI.factory.getEntryRetrievalService();
 
-        //Retrieve UniProt entry by its accession number
-	String acc = accession;
-	String accessionPTMInfo = "";
-	List<String> uniprotDetails = new ArrayList<String>(); 
-	//UniProtEntry entry = new UniProtEntry();
-        /* UniProtEntry entry = (UniProtEntry) entryRetrievalService.getUniProtEntry(acc);	
+		ArrayList arrayFunction = new ArrayList();
+		List arrayPTM = new ArrayList();
+		//Create entry retrieval service
+		EntryRetrievalService entryRetrievalService = UniProtJAPI.factory.getEntryRetrievalService();
+
+		//Retrieve UniProt entry by its accession number
+		String acc = accession;
+		String accessionPTMInfo = "";
+		List<String> uniprotDetails = new ArrayList<String>(); 
+		//UniProtEntry entry = new UniProtEntry();
+		/* UniProtEntry entry = (UniProtEntry) entryRetrievalService.getUniProtEntry(acc);	
 	ProteinDataQueryService queryService = UniProtJAPI.factory.getProteinDataQueryService();
 
-                
+
         uk.ac.ebi.kraken.uuw.services.remoting.Query proteinQuery = UniProtQueryBuilder.buildQuery(acc);
         EntryIterator<ProteinData> entries = queryService.getEntryIterator(proteinQuery);
         System.out.println("Entries with protein name quried = " + entries.getResultSize());
@@ -82,7 +81,7 @@ public class UniprotConnection extends Controller {
                                 String ptm = "PTM";
 
                                 String uniprot = com.getCommentType().toString();
-                                
+
                                 if (uniprot.equals(function)) {
 
                                         arrayFunction.add(com.toString());
@@ -95,111 +94,239 @@ public class UniprotConnection extends Controller {
                         }
 	}*/
 
-	try {
-		Object accessionPTM = UniProtJAPI.factory.getEntryRetrievalService().getUniProtAttribute(accession,  "ognl:getComments(@uk.ac.ebi.kraken.interfaces.uniprot.comments.CommentType@PTM).{value}" );
-		accessionPTMInfo = accessionPTM.toString();
-		accessionPTMInfo = accessionPTMInfo.replaceAll("^\\[", "");
-		accessionPTMInfo = accessionPTMInfo.replaceAll("]$", "");
-		uniprotDetails.add(accessionPTMInfo); 
-	} catch (RemoteDataAccessException e) {
-    		System.err.println("Caught IOException: " + e.getMessage());
-	}
-	
+		try {
+			Object accessionPTM = UniProtJAPI.factory.getEntryRetrievalService().getUniProtAttribute(accession,  "ognl:getComments(@uk.ac.ebi.kraken.interfaces.uniprot.comments.CommentType@PTM).{value}" );
+			accessionPTMInfo = accessionPTM.toString();
+			accessionPTMInfo = accessionPTMInfo.replaceAll("^\\[", "");
+			accessionPTMInfo = accessionPTMInfo.replaceAll("]$", "");
+			uniprotDetails.add(accessionPTMInfo); 
+		} catch (RemoteDataAccessException e) {
+			System.err.println("Caught IOException: " + e.getMessage());
+		}
 
-	return uniprotDetails;
+
+		return uniprotDetails;
 	}
-	
-	
-	
+
+
+
 	public static String EntryRetrievalSequence(String accession) {
 
-        EntryRetrievalService entryRetrievalService = UniProtJAPI.factory.getEntryRetrievalService();
-	String sequenceCat = "";
+		EntryRetrievalService entryRetrievalService = UniProtJAPI.factory.getEntryRetrievalService();
+		String sequenceCat = "";
 
-	try {
-        Object attribute = UniProtJAPI.factory.getEntryRetrievalService().getUniProtAttribute(accession ,  "ognl:sequence.value");
+		try {
+			Object attribute = UniProtJAPI.factory.getEntryRetrievalService().getUniProtAttribute(accession ,  "ognl:sequence.value");
 
-        String sequence = attribute.toString();
+			String sequence = attribute.toString();
 
-        String []thisCombo2 = sequence.split("(?<=\\G..........)");
-        String asString = Arrays.toString(thisCombo2);
-        int i = 0;
-        for(String s : thisCombo2) {
-                if (i == 50 ) {
-                sequenceCat += s;
-                //sequenceCat += "\n";
-                i = 0;
-                } else {
-                sequenceCat += s;
-                i = i + 10;
-                }
-        }
+			String []thisCombo2 = sequence.split("(?<=\\G..........)");
+			String asString = Arrays.toString(thisCombo2);
+			int i = 0;
+			for(String s : thisCombo2) {
+				if (i == 50 ) {
+					sequenceCat += s;
+					//sequenceCat += "\n";
+					i = 0;
+				} else {
+					sequenceCat += s;
+					i = i + 10;
+				}
+			}
 
-	} catch (RemoteDataAccessException e) {
-                System.err.println("Caught IOException: " + e.getMessage());
-        }
+		} catch (RemoteDataAccessException e) {
+			System.err.println("Caught IOException: " + e.getMessage());
+		}
 
 
-        return sequenceCat;
+		return sequenceCat;
 	}
 
 
 	public static Result proteinsite() {
 
-        String proteinName = "";
-        String swissProtName = "";
-	String proteinNameFull = "";
-        String site = "";
-        String type = "";
-	String sequenceRetrieval = "";
-        String protein = "";
+		String proteinName = "";
+		String swissProtName = "";
+		String proteinNameFull = "";
+		String site = "";
+		String type = "";
+		String sequenceRetrieval = "";
+		String protein = "";
 
-	ArrayList<SqlRow> biolRefs  = new ArrayList<SqlRow>();
-        List<StructureToSiteDefined> definedStructures = null;
-        List<GeneralSites> generalStructures = null;
-        ArrayList<Long> structuresShow = new ArrayList();
-        List<SqlRow> listSql = null;
-        ArrayList<SqlRow> listSqlArray = new ArrayList<SqlRow>();
-        ArrayList<Biolsource> biolSourceProtein = new ArrayList<Biolsource>();
-        List<Biolsource> biolSourceProteins = Biolsource.findBiolSourceIds(protein);
-        HashSet taxsources = new HashSet();
-        ArrayList taxsourcesUnique = new ArrayList();
-		
-        if (request().queryString().size() > 0  ) {
-                Map<String, String[]> params = request().queryString();
-                String[] searchTerms = null;
-                
-		
-                String key = null;
-                for (Map.Entry<String, String[]> entry : params.entrySet() ){
-                        key = entry.getKey();
-                        searchTerms = entry.getValue();
-                
-                if(key.equals("position")) {
-			for(String p : searchTerms) {
-			site = p;
-                        System.out.println(" -------> output is " + site);
-			}
-                }
-                if(key.equals("protein")) {
-			for(String p : searchTerms) {
-			protein = p;
-                        System.out.println("output is 2 " + protein);
-			//proteinNameFull = GsProteinSite.ProteinRetrievalName(protein);
-			/* for(GsProteinSite g : proteinSummary) {
-				String proteinNameFu2ll = g.protein_name;
-			}*/
-				
-			}
-                }
+		ArrayList<SqlRow> biolRefs  = new ArrayList<SqlRow>();
+		List<StructureToSiteDefined> definedStructures = null;
+		List<GeneralSites> generalStructures = null;
+		ArrayList<Long> structuresShow = new ArrayList();
+		List<SqlRow> listSql = null;
+		ArrayList<SqlRow> listSqlArray = new ArrayList<SqlRow>();
+		ArrayList<Biolsource> biolSourceProtein = new ArrayList<Biolsource>();
+		List<Biolsource> biolSourceProteins = Biolsource.findBiolSourceIds(protein);
+		HashSet taxsources = new HashSet();
+		ArrayList taxsourcesUnique = new ArrayList();
 
-		if(key.equals("type")){
-			for(String t : searchTerms) {
-			type = t;
+		if (request().queryString().size() > 0  ) {
+			Map<String, String[]> params = request().queryString();
+			String[] searchTerms = null;
+			List<Ftmerge> fts = null;
+
+			String key = null;
+
+			for (Map.Entry<String, String[]> entry : params.entrySet() ){
+				key = entry.getKey();
+				searchTerms = entry.getValue();
+
+				//from swissProt redirect
+				if(key.equals("feature")) {
+					for(String ft : searchTerms){
+
+						fts = Ftmerge.findFt(ft);
+					}
+
+					for(Ftmerge f : fts ) {
+						protein = f.swiss_prot;
+						site = f.amino_acid;
+					}
+				}
+
+				if(key.equals("position")) {
+					for(String p : searchTerms) {
+						site = p;
+						System.out.println(" -------> output is " + site);
+					}
+				}
+				if(key.equals("protein")) {
+					for(String p : searchTerms) {
+						protein = p;
+						System.out.println("output is 2 " + protein);
+					}
+				}
+
+				if(key.equals("type")){
+					for(String t : searchTerms) {
+						type = t;
+					}
+				}
+
 			}
+
+			if(type.equals("defined")) {
+				definedStructures = StructureToSiteDefined.findStructuresDefined(protein, site);
+				int svalue;
+				int x;
+				for(StructureToSiteDefined s : definedStructures) {
+					x = s.structure_id;
+					Long value = Long.valueOf(x);
+					//this check should not be required but for safety
+					if(!structuresShow.contains(value)) {
+						structuresShow.add(value);
+					}
+				}
+			}
+
+			else if (type.equals("general")) {
+				int gvalue;
+				int x;
+				generalStructures = GeneralSites.findStructuresGeneral(protein, site);
+				for(GeneralSites str : generalStructures) {
+					List<StructureToSiteGeneral> general = str.strSiteGeneral;
+					for(StructureToSiteGeneral g : general) {
+						if(g.structure_id  > 0){
+							gvalue = g.structure_id;
+							Long value = Long.valueOf(gvalue);
+							//this check should not be required but for safety
+							if(!structuresShow.contains(value)) {
+								structuresShow.add(value);
+							}
+						}
+					}
+				}
+				for(Long s : structuresShow) {
+					System.out.println("check values" + s );
+				}
+			}
+
+			for(Biolsource biol : biolSourceProteins){
+				swissProtName = biol.swiss_prot;
+				proteinName = biol.protein;
+				Biolsource objectBiolSource = Ebean.find(Biolsource.class, biol.id);
+				taxsources.add(objectBiolSource.taxonomy);
+				biolSourceProtein.add(objectBiolSource);
+			}
+
+			taxsourcesUnique.addAll(taxsources);	
+
+			biolRefs = Biolsource.findBiolsourceRefs(protein);
+
+			sequenceRetrieval = UniprotConnection.EntryRetrievalSequence(protein);
+
 		}
 
+		Object format = Cache.get("format");
+		String notation = "gs";
+		if(format != null) {notation = (String) format.toString();}
+
+		System.out.println("xxxxxxxxxx" + protein + " ssssssssssssss" + site + " 11 " + sequenceRetrieval.length() + " 22 " + protein.length() + " 33 " + biolRefs.size() + " 4 " + site.length() + " 5 " + structuresShow.size() + " 6 x" + taxsourcesUnique.size());
+
+		return ok(
+				proteinsite.render(notation, sequenceRetrieval,  protein, biolRefs, site, structuresShow,  taxsourcesUnique )
+				);
+	}
+
+	/*public static Result swissprotFT( String ft) {
+		HashSet<Reference> references = new HashSet<Reference>();
+
+		List<Ftmerge> fts = null;
+		fts = Ftmerge.findFt(ft);
+
+
+		//ideal we need to send user to the page expecting see JIRA
+		for(Ftmerge f : fts ) {
+			String swiss = f.swiss_prot;
+			String position = f.amino_acid;
+
+			Long refid = f.reference.id;
+			Reference reference = Ebean.find(Reference.class, refid);
+			references.add(reference);
 		}
+		//http://unicarbkb.org/proteinsite?position=ASN-516&protein=P43237&type=defined ?;
+
+		return ok(swissprotFT.render(references, ft));
+	}*/
+
+	//this needs some tidying 
+	public static Result swissprotFT( String ft ) {
+		//redirect("/proteinsite");
+		//return redirect(controllers.UniprotConnection.proteinsite());
+		String proteinName = "";
+		String swissProtName = "";
+		String proteinNameFull = "";
+		String site = "";
+		String type = "";
+		String sequenceRetrieval = "";
+		String protein = "";
+
+		ArrayList<SqlRow> biolRefs  = new ArrayList<SqlRow>();
+		List<StructureToSiteDefined> definedStructures = null;
+		List<GeneralSites> generalStructures = null;
+		ArrayList<Long> structuresShow = new ArrayList();
+		List<SqlRow> listSql = null;
+		ArrayList<SqlRow> listSqlArray = new ArrayList<SqlRow>();
+		ArrayList<Biolsource> biolSourceProtein = new ArrayList<Biolsource>();
+		List<Biolsource> biolSourceProteins = Biolsource.findBiolSourceIds(protein);
+		HashSet taxsources = new HashSet();
+		ArrayList taxsourcesUnique = new ArrayList();
+
+		List<Ftmerge> fts;
+
+		fts = Ftmerge.findFt(ft);
+
+
+		for(Ftmerge f : fts ) {
+			protein = f.swiss_prot;
+			site = f.amino_acid;
+		}
+
+		type = "defined";
 
 		if(type.equals("defined")) {
 			definedStructures = StructureToSiteDefined.findStructuresDefined(protein, site);
@@ -208,10 +335,10 @@ public class UniprotConnection extends Controller {
 			for(StructureToSiteDefined s : definedStructures) {
 				x = s.structure_id;
 				Long value = Long.valueOf(x);
-					//this check should not be required but for safety
-					if(!structuresShow.contains(value)) {
-                                        	structuresShow.add(value);
-                                      	}
+				//this check should not be required but for safety
+				if(!structuresShow.contains(value)) {
+					structuresShow.add(value);
+				}
 			}
 		}
 
@@ -223,60 +350,52 @@ public class UniprotConnection extends Controller {
 				List<StructureToSiteGeneral> general = str.strSiteGeneral;
 				for(StructureToSiteGeneral g : general) {
 					if(g.structure_id  > 0){
-					gvalue = g.structure_id;
-					Long value = Long.valueOf(gvalue);
+						gvalue = g.structure_id;
+						Long value = Long.valueOf(gvalue);
 						//this check should not be required but for safety
 						if(!structuresShow.contains(value)) {
-						structuresShow.add(value);
+							structuresShow.add(value);
 						}
 					}
 				}
 			}
 			for(Long s : structuresShow) {
-			System.out.println("check values" + s );
+				System.out.println("check values" + s );
 			}
 		}
 
-                for(Biolsource biol : biolSourceProteins){
-                        proteinName = biol.protein;
-                        swissProtName = biol.swiss_prot;
-                        Biolsource objectBiolSource = Ebean.find(Biolsource.class, biol.id);
+		for(Biolsource biol : biolSourceProteins){
+			swissProtName = biol.swiss_prot;
+			proteinName = biol.protein;
+			Biolsource objectBiolSource = Ebean.find(Biolsource.class, biol.id);
 			taxsources.add(objectBiolSource.taxonomy);
-                        biolSourceProtein.add(objectBiolSource);
-                }
-		
+			biolSourceProtein.add(objectBiolSource);
+		}
+
 		taxsourcesUnique.addAll(taxsources);	
-		
+
 		biolRefs = Biolsource.findBiolsourceRefs(protein);
-		
+
 		sequenceRetrieval = UniprotConnection.EntryRetrievalSequence(protein);
 
-        }
 
-	Object format = Cache.get("format");
-        String notation = "gs";
-        if(format != null) {notation = (String) format.toString();}
+		Object format = Cache.get("format");
+		String notation = "gs";
+		if(format != null) {notation = (String) format.toString();}
 
-        return ok(
-                proteinsite.render(notation, sequenceRetrieval,  protein, biolRefs, site, structuresShow,  taxsourcesUnique )
-                );
-    	}
 
-	public static Result swissprotFT( String ft) {
-		HashSet<Reference> references = new HashSet<Reference>();
+		return ok(
+				proteinsite.render(notation, sequenceRetrieval,  protein, biolRefs, site, structuresShow,  taxsourcesUnique )
+				);
 
-		System.out.println("swissprot ft check " + ft);
-		List<Ftmerge> fts = null;
-		fts = Ftmerge.findFt(ft);
-		for(Ftmerge f : fts ) {
-			System.out.println("jsjsjsjsj " + f.reference.id);
-			Long refid = f.reference.id;
-			Reference reference = Ebean.find(Reference.class, refid);
-			references.add(reference);
-		}
-		
-	return ok(swissprotFT.render(references, ft));
 	}
 
-	
+
+
+	private static Result redirect(Result proteinsite) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
